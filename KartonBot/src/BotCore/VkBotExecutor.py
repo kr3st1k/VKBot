f8607af4d70e80b971121fc1e3d61c8d73615ba6
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-11-25 21:25:20 +0300

    Add files via upload

---
+++ KartonBot/src/BotCore/VkBotExecutor.py
@@ -0,0 +1,23 @@
+import BotCore.VkCommands
+
+class VkBotExecutor(VkCommands):
+    def __init__(self):
+        super().__init__()
+    def invite(self):
+        pass
+    def ban(self):
+        pass
+    def add_command(self):
+        pass
+    def delete_command(self):
+        pass
+    def roll(self):
+        pass
+    def chance(self):
+        pass
+    def add_multi_command(self):
+        pass
+    def suicide(self):
+        pass
+    def time(self):
+        pass
